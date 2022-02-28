5c89b33a705b46c9900592a9e30536b9626241d9
Facupelli <facundopellicer4@gmail.com>
2022-02-28 19:10:07 -0300

    added work titles

--- client/src/components/Works/Works.jsx
+++ client/src/components/Works/Works.jsx
@@ -56,10 +56,6 @@ export default function Works({ divRef, setLoadImage2 }) {
     setSeeMoreCuyo(!seeMoreCuyo);
   };
 
-  const handleOnClickChat = () => {
-    setSeeMoreChat(!seeMoreChat);
-  };
-
   const handleOnClickAlkemy = () => {
     setSeeMoreAlkemy(!seeMoreAlkemy);
   };
@@ -70,6 +66,10 @@ export default function Works({ divRef, setLoadImage2 }) {
 
   const handleOnClickCognizant = () => {
     setSeeMoreCognizant(!seeMoreCognizant);
+  };
+
+  const handleOnClickChat = () => {
+    setSeeMoreChat(!seeMoreChat);
   };
 
   return (
@@ -88,52 +88,42 @@ export default function Works({ divRef, setLoadImage2 }) {
         />
       </div>
 
-      {/* ----------------------------------- DOGS CARD ------------------------------------------ */}
-      <Fade triggerOnce>
-        <div className="flex justify-center">
-          {!seeMoreDogs && (
-            <div className={s.image}>
-              <img className={s.image__img} src={dogImage} alt="dogs" />
-              <div className={s.image__overlay}>
-                <button onClick={handleOnClickDog}>
-                  <div className={s.image__title}>
-                    <div>SEE MORE</div>
-                    <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
-                  </div>
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-      </Fade>
-
-      {/* ----------------------------------- DESPLIEGUE DOGS ------------------------------------------ */}
-
-      {seeMoreDogs && (
-        <div className="">
-          <Dogs setSeeMoreDogs={setSeeMoreDogs} seeMoreDogs={seeMoreDogs} />
-        </div>
-      )}
-
       {/* ----------------------------------- WEBSERVICE CARD ------------------------------------------ */}
 
-      <Fade triggerOnce>
-        <div className="mt-24 flex justify-center">
-          {!seeMoreWeb && (
-            <div className={s.image}>
-              <img className={s.image__img} src={webImage} alt="webservice" />
-              <div className={s.image__overlay}>
-                <button onClick={handleOnClickWeb}>
-                  <div className={s.image__title}>
-                    <div>SEE MORE</div>
-                    <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
-                  </div>
-                </button>
-              </div>
+      <div className="grid grid-cols-3 gap-x-12 items-center ">
+        {!seeMoreWeb && (
+          <div className="hidden md:col-span-1 md:flex justify-end font-bold  text-white text-3xl text-center">
+            <div className="">
+              <p>SERVICES</p>
+              <p>E-COMMERCE</p>
+              <p className="text-xl">buy and offer services</p>
             </div>
-          )}
+          </div>
+        )}
+        <div className="col-span-3 md:col-span-2 ">
+          <Fade triggerOnce>
+            <div className=" flex justify-center md:justify-start">
+              {!seeMoreWeb && (
+                <div className={s.image}>
+                  <img
+                    className={s.image__img}
+                    src={webImage}
+                    alt="webservice"
+                  />
+                  <div className={s.image__overlay}>
+                    <button onClick={handleOnClickWeb}>
+                      <div className={s.image__title}>
+                        <div>SEE MORE</div>
+                        <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Fade>
         </div>
-      </Fade>
+      </div>
 
       {/* ----------------------------------- DESPLIEGUE WEBSERVICE ------------------------------------------ */}
 
@@ -145,23 +135,37 @@ export default function Works({ divRef, setLoadImage2 }) {
 
       {/* ----------------------------------- CUYOBREWERS CARD ------------------------------------------ */}
 
-      <Fade triggerOnce>
-        <div className="mt-24 flex justify-center">
-          {!seeMoreCuyo && (
-            <div className={s.image}>
-              <img className={s.image__img} src={home} alt="webservice" />
-              <div className={s.image__overlay}>
-                <button onClick={handleOnClickCuyo}>
-                  <div className={s.image__title}>
-                    <div>SEE MORE</div>
-                    <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
-                  </div>
-                </button>
-              </div>
+      <div className="grid grid-cols-3 gap-x-12 items-center  ">
+        {!seeMoreCuyo && (
+          <div className="hidden md:col-span-1 md:flex justify-end font-bold  text-white  text-center">
+            <div className="mt-24">
+              <p className="text-3xl">HOMEBREW APP</p>
+              <p className="text-3xl">RECIPE BUILDER</p>
+              <p className="text-lg">post, comment and</p>
+              <p className="text-lg">follow beer recipes</p>
             </div>
-          )}
+          </div>
+        )}
+        <div className="col-span-3 md:col-span-2">
+          <Fade triggerOnce>
+            <div className="mt-24 flex justify-center md:justify-start ">
+              {!seeMoreCuyo && (
+                <div className={s.image}>
+                  <img className={s.image__img} src={home} alt="webservice" />
+                  <div className={s.image__overlay}>
+                    <button onClick={handleOnClickCuyo}>
+                      <div className={s.image__title}>
+                        <div>SEE MORE</div>
+                        <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Fade>
         </div>
-      </Fade>
+      </div>
 
       {/* ----------------------------------- DESPLIEGUE CUYOBREWRES ------------------------------------------ */}
 
@@ -174,6 +178,47 @@ export default function Works({ divRef, setLoadImage2 }) {
         </div>
       )}
 
+      {/* ----------------------------------- DOGS CARD ------------------------------------------ */}
+
+      <div className="grid grid-cols-3 gap-x-12 items-center  ">
+        {!seeMoreDogs && (
+          <div className="hidden md:col-span-1 md:flex justify-end font-bold  text-white text-3xl text-center">
+            <div className="mt-24">
+              <p>DOG APP</p>
+              <p className="text-3xl">API CONSUMER</p>
+              <p className="text-xl">thedogapi.com</p>
+            </div>
+          </div>
+        )}
+        <div className="col-span-3 md:col-span-2">
+          <Fade triggerOnce>
+            <div className="mt-24 flex justify-center md:justify-start ">
+              {!seeMoreDogs && (
+                <div className={s.image}>
+                  <img className={s.image__img} src={dogImage} alt="dogs" />
+                  <div className={s.image__overlay}>
+                    <button onClick={handleOnClickDog}>
+                      <div className={s.image__title}>
+                        <div>SEE MORE</div>
+                        <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Fade>
+        </div>
+      </div>
+
+      {/* ----------------------------------- DESPLIEGUE DOGS ------------------------------------------ */}
+
+      {seeMoreDogs && (
+        <div className="">
+          <Dogs setSeeMoreDogs={setSeeMoreDogs} seeMoreDogs={seeMoreDogs} />
+        </div>
+      )}
+
       {/* ------------------------------------- QUOTTE ----------------------------------------------------- */}
 
       <div className="mt-32 hidden md:block ">
@@ -182,23 +227,40 @@ export default function Works({ divRef, setLoadImage2 }) {
 
       {/* ----------------------------------- ALKEMY CARD ------------------------------------------ */}
 
-      <Fade triggerOnce>
-        <div className="mt-32 flex justify-center">
-          {!seeMoreAlkemy && (
-            <div className={s.image}>
-              <img className={s.image__img} src={homeAlkemy} alt="chat_room" />
-              <div className={s.image__overlay}>
-                <button onClick={handleOnClickAlkemy}>
-                  <div className={s.image__title}>
-                    <div>SEE MORE</div>
-                    <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
-                  </div>
-                </button>
-              </div>
+      <div className="grid grid-cols-3 gap-x-12 items-center  ">
+        {!seeMoreAlkemy && (
+          <div className="hidden md:col-span-1 md:flex justify-end font-bold  text-white  text-center">
+            <div className="md:mt-32">
+              <p className="text-3xl">HOTEL MENU APP</p>
+              <p className="text-lg">Create the menu where</p>
+              <p className="text-lg">2 plates have to be vegan</p>
             </div>
-          )}
+          </div>
+        )}
+        <div className="col-span-3 md:col-span-2">
+          <Fade triggerOnce>
+            <div className="mt-24 md:mt-32 flex justify-center md:justify-start ">
+              {!seeMoreAlkemy && (
+                <div className={s.image}>
+                  <img
+                    className={s.image__img}
+                    src={homeAlkemy}
+                    alt="chat_room"
+                  />
+                  <div className={s.image__overlay}>
+                    <button onClick={handleOnClickAlkemy}>
+                      <div className={s.image__title}>
+                        <div>SEE MORE</div>
+                        <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Fade>
         </div>
-      </Fade>
+      </div>
 
       {/* ----------------------------------- ALKEMY DESPLIEGUE ------------------------------------------ */}
 
@@ -212,24 +274,40 @@ export default function Works({ divRef, setLoadImage2 }) {
       )}
 
       {/* ----------------------------------- TODO-APP CARD ------------------------------------------ */}
-
-      <Fade triggerOnce>
-        <div className="mt-24 flex justify-center">
-          {!seeMoreTodo && (
-            <div className={s.image}>
-              <img className={s.image__img} src={homeTodo1} alt="chat_room" />
-              <div className={s.image__overlay}>
-                <button onClick={handleOnClickTodo}>
-                  <div className={s.image__title}>
-                    <div>SEE MORE</div>
-                    <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
-                  </div>
-                </button>
-              </div>
+      <div className="grid grid-cols-3 gap-x-12 items-center  ">
+        {!seeMoreTodo && (
+          <div className="hidden md:col-span-1 md:flex justify-end font-bold  text-white  text-center">
+            <div className="mt-24">
+              <p className="text-3xl">TODO APP</p>
+              <p className="text-lg">create folders,post to-do</p>
+              <p className="text-lg">mark them as complete</p>
             </div>
-          )}
+          </div>
+        )}
+        <div className="col-span-3 md:col-span-2">
+          <Fade triggerOnce>
+            <div className="mt-24 flex justify-center md:justify-start">
+              {!seeMoreTodo && (
+                <div className={s.image}>
+                  <img
+                    className={s.image__img}
+                    src={homeTodo1}
+                    alt="chat_room"
+                  />
+                  <div className={s.image__overlay}>
+                    <button onClick={handleOnClickTodo}>
+                      <div className={s.image__title}>
+                        <div>SEE MORE</div>
+                        <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Fade>
         </div>
-      </Fade>
+      </div>
 
       {/* ----------------------------------- TODO-APP DESPLIEGUE ------------------------------------------ */}
 
@@ -241,27 +319,40 @@ export default function Works({ divRef, setLoadImage2 }) {
 
       {/* ----------------------------------- COGNIZANT CHALLENGE CARD ------------------------------------------ */}
 
-      <Fade triggerOnce>
-        <div className="mt-24 flex justify-center">
-          {!seeMoreCognizant && (
-            <div className={s.image}>
-              <img
-                className={s.image__img}
-                src={homeCognizant}
-                alt="chat_room"
-              />
-              <div className={s.image__overlay}>
-                <button onClick={handleOnClickCognizant}>
-                  <div className={s.image__title}>
-                    <div>SEE MORE</div>
-                    <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
-                  </div>
-                </button>
-              </div>
+      <div className="grid grid-cols-3 gap-x-12 items-center  ">
+        {!seeMoreCognizant && (
+          <div className="hidden md:col-span-1 md:flex justify-end font-bold  text-white  text-center">
+            <div className="mt-24">
+              <p className="text-3xl">COGNIZANT</p>
+              <p className="text-3xl">CHALLENGE</p>
+              <p className="text-lg">interview process app</p>
             </div>
-          )}
+          </div>
+        )}
+        <div className="col-span-3 md:col-span-2">
+          <Fade triggerOnce>
+            <div className="mt-24 flex justify-center md:justify-start">
+              {!seeMoreCognizant && (
+                <div className={s.image}>
+                  <img
+                    className={s.image__img}
+                    src={homeCognizant}
+                    alt="chat_room"
+                  />
+                  <div className={s.image__overlay}>
+                    <button onClick={handleOnClickCognizant}>
+                      <div className={s.image__title}>
+                        <div>SEE MORE</div>
+                        <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
+                      </div>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Fade>
         </div>
-      </Fade>
+      </div>
 
       {/* ----------------------------------- COGNIZANT CHALLENGE DESPLIEGUE ------------------------------------------ */}
 
@@ -276,29 +367,41 @@ export default function Works({ divRef, setLoadImage2 }) {
 
       {/* ----------------------------------- CHAT-ROOM CARD ------------------------------------------ */}
 
-      <Fade triggerOnce>
-        <div className="mt-24 flex justify-center md:pb-32">
-          {!seeMoreChat && (
-            <div className={s.image}>
-              <img className={s.image__img} src={chat} alt="chat_room" />
-              <div className={s.image__overlay}>
-                {/* <button onClick={handleOnClickCuyo}> */}
-                <a
-                  href="https://github.com/Facupelli/chat-room"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <div className={s.image__title}>
-                    <div>SEE MORE</div>
-                    <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
-                  </div>
-                </a>
-                {/* </button> */}
-              </div>
+      <div className="grid grid-cols-3 gap-x-12 items-center  ">
+        {!seeMoreCognizant && (
+          <div className="hidden md:col-span-1 md:flex justify-end font-bold  text-white  text-center">
+            <div className="">
+              <p className="text-3xl">CHAT ROOM APP</p>
+              <p className="text-lg">currently working on it</p>
             </div>
-          )}
+          </div>
+        )}
+        <div className="col-span-3 md:col-span-2">
+          <Fade triggerOnce>
+            <div className="mt-24 flex justify-center md:justify-start md:pb-32">
+              {!seeMoreChat && (
+                <div className={s.image}>
+                  <img className={s.image__img} src={chat} alt="chat_room" />
+                  <div className={s.image__overlay}>
+                    {/* <button onClick={handleOnClickCuyo}> */}
+                    <a
+                      href="https://github.com/Facupelli/chat-room"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <div className={s.image__title}>
+                        <div>SEE MORE</div>
+                        <ArrowDownIcon className="h-6 w-6 text-cyan-400" />
+                      </div>
+                    </a>
+                    {/* </button> */}
+                  </div>
+                </div>
+              )}
+            </div>
+          </Fade>
         </div>
-      </Fade>
+      </div>
 
       {/* ----------------------------------- CHAT-ROOM DESPLIEGUE ------------------------------------------ */}
 
@@ -318,12 +421,6 @@ export default function Works({ divRef, setLoadImage2 }) {
         </Fade>
       </div>
 
-      <div>
-        {/* <Dogs /> */}
-        {/* <Love /> */}
-        {/* <WebService /> */}
-        {/* <Matters /> */}
-      </div>
     </div>
   );
 }
